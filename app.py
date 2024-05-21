b5b6c9913a31a50f9cae8fb0cea10065a55016ad
Seyr Naser <naserseyr@gmail.com>
2024-05-21 16:14:04 -0400

    Updated app.py and template files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import subprocess
 import shlex
 
@@ -12,24 +12,25 @@ def home():
 def camera1():
     return render_template('camera1.html')
 
-@app.route('/start-hls', methods=['POST'])
-def start_hls():
+@app.route('/start-streams', methods=['POST'])
+def start_streams():
     script_path = "/home/seyr/Desktop/start_camera1.py"
     python_env_path = "/home/seyr/myprojectenv/bin/python3"
     try:
-        # Start the RTSP stream script without waiting for it to finish
-        rtsp_process = subprocess.Popen([python_env_path, script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen([python_env_path, script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ffmpeg_command = "sudo /usr/bin/ffmpeg -i rtsp://10.0.32.250:8554/stream1 -codec copy -bsf:v h264_mp4toannexb -hls_time 2 -hls_list_size 10 -hls_flags delete_segments+append_list -f hls /var/www/html/hls/camera1/index.m3u8"
+        subprocess.Popen(shlex.split(ffmpeg_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return jsonify({'success': True, 'message': 'Stream started successfully.'})
+    except Exception as e:
+        return jsonify({'success': False, 'message': 'Failed to start the stream.', 'error': str(e)})
 
-        # Clear previous HLS files
-        subprocess.run(['sudo', 'rm', '-rf', '/var/www/html/hls/camera1/*'], check=True)
-
-        # Start the HLS conversion
-        ffmpeg_command = "sudo /usr/bin/ffmpeg -i rtsp://10.0.32.250:8554/stream1 -codec copy -bsf:v h264_mp4toannexb -hls_time 10 -hls_list_size 0 -hls_flags delete_segments+append_list -f hls /var/www/html/hls/camera1/index.m3u8"
-        ffmpeg_process = subprocess.Popen(shlex.split(ffmpeg_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        return jsonify({'success': True, 'message': 'HLS stream started successfully.'})
-    except Exception as e:  # Catching a general exception to handle unexpected errors
-        return jsonify({'success': False, 'message': 'Failed to execute streaming commands.', 'error': str(e)})
+@app.route('/reset-hls', methods=['POST'])
+def reset_hls():
+    try:
+        subprocess.run('sudo rm /var/www/html/hls/*/*.m3u8 /var/www/html/hls/*/*.ts', shell=True, check=True)
+        return jsonify({'success': True, 'message': 'All streams reset successfully.'})
+    except subprocess.CalledProcessError as e:
+        return jsonify({'success': False, 'message': 'Failed to reset streams.', 'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
